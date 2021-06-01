beb1b1c5db4616b103f5404ee75e4fc4979546a8
ko-suke1016 <program8910@gmail.com>
2021-06-01 19:18:13 +0900

    Fix リファクタリング

--- app/javascript/packs/graph/mental.js
+++ app/javascript/packs/graph/mental.js
@@ -34,45 +34,13 @@ var myChart = new Chart(ctx, {
     }
 });
 
-// userのphisical_gageカラムの値によって処理が実行されないようにする
-// var mental_gage_score = physicals.join(',');
-// console.log(mental_gage_score);
 
-// third_question(簡単タスク)実行後メンタル値の減少
-// if (mental_gage_score>5){
-// $(".simple_task").on('click',function(){
-//     console.log('neko');
-//     var target = $(this);
-//     var result = target.data("send-id");
-//     myChart.data.datasets[0].data = myChart.data.datasets[0].data.map(function(X){return X - result})
-//     myChart.update();
-// });
-// }
-
-// メンタルゲージの残りに応じて質問に答えられなくなる
-
-// $('.long-task').on('click',function(){
-//     if (myChart.data.datasets[0].data<20) {
-//         // ajaxで送信処理
-//         $.ajax({
-//             type: "get",
-//             url: "less_mental",
-//             detaType: "json"
-//         })
-//         .done(function(){
-//             console.log("tako");
-//         })
-//     }
-// })
-// // first_question(基礎編)実行後メンタル値の減少
-// if (mental_gage_score>10){
+// first_question(基礎編)実行後メンタル値減少
 $(".first_question_start").on('click',function(){
     var result = 10;
     myChart.data.datasets[0].data = myChart.data.datasets[0].data.map(function(X){return X - result})
     myChart.update();
 });
-
-
 
 // second_question(実践編)実行後メンタル値減少
 $(".second_question_start").on('click',function(){
