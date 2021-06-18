597997a0d322bcd3d36fa29a9dc31017e2400787
ko-suke1016 <program8910@gmail.com>
2021-06-18 20:55:15 +0900

    Add 本番環境でのgmailの使用記述

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,6 +89,20 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: 'gmail.com',
+    #-----変更点---------------------------
+    user_name: ENV['SMTP_USERNAME'],
+    password: ENV['SMTP_PASSWORD'],
+    #----変更点ここまで-----------------------
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
+
   # Inserts middleware to perform automatic connection switching.
   # The `database_selector` hash is used to pass options to the DatabaseSelector
   # middleware. The `delay` is used to determine how long to wait after a write
