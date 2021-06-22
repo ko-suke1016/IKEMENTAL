1f9498f7b9356a2387b5018b97942b47d4c52ce1
ko-suke1016 <program8910@gmail.com>
2021-06-23 02:33:42 +0900

    Fix お問い合わせメール修正

--- app/mailers/inquiry_mailer.rb
+++ app/mailers/inquiry_mailer.rb
@@ -1,6 +1,6 @@
 class InquiryMailer < ApplicationMailer
     def send_mail(inquiry)
         @inquiry = inquiry
-        mail(to: ENV['TOMAIL'], subject: "お問い合わせ")
+        mail (to: ENV['GMAIL_USERNAME'], subject: "お問い合わせ")
     end
 end
